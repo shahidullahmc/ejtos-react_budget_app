d124af542e137885e31e25280680e2271de763c0
shahidullahmc <designer.shahidullah@outlook.com>
2023-08-01 04:37:01 -0400

    CHANGE FOR RECAT FRONT-END APP PROJECT

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,94 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+  const { budget, setBudget, expenses } = useContext(AppContext);
+  const upperLimit = 20000;
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
+  const [editableBudget, setEditableBudget] = useState(totalExpenses);
+  const [currency, setCurrency] = useState('£'); // Set the default currency to Pound (£)
+
+  useEffect(() => {
+    setEditableBudget(totalExpenses);
+  }, [totalExpenses]);
+
+  const handleAmountChange = (amount) => {
+    const newBudget = editableBudget + amount;
+    const clampedBudget = Math.min(Math.max(newBudget, totalExpenses), upperLimit);
+    setEditableBudget(clampedBudget);
+    setBudget(clampedBudget); // Update the budget in the context
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setEditableBudget(Number(value));
+  };
+
+  const handleBlur = () => {
+    const clampedBudget = Math.min(Math.max(editableBudget, totalExpenses), upperLimit);
+    setEditableBudget(clampedBudget);
+    setBudget(clampedBudget); // Update the budget in the context
+  };
+
+  const handleCurrencyChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <div
+      className='alert alert-secondary'
+      style={{ padding: '10px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
+    >
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <span>
+          Budget: {currency} {budget}
+        </span>
+        <div>
+          <button onClick={() => handleAmountChange(10)}>▲</button>
+          <button onClick={() => handleAmountChange(-10)}>▼</button>
+          <select
+            value={currency}
+            onChange={(e) => {
+              handleCurrencyChange(e);
+            }}
+            style={{
+              appearance: 'none',
+              background: 'url(https://image-url.com/dropdown-icon.png) no-repeat right center',
+              backgroundSize: '20px 20px',
+              padding: '5px 25px 5px 10px',
+              borderRadius: '3px',
+              border: '1px solid #ccc',
+              cursor: 'pointer',
+              marginLeft: '5px',
+            }}
+          >
+            <option value='£'>£ Pound</option>
+            <option value='$'>$ Dollar</option>
+            <option value='€'>€ Euro</option>
+            <option value='₹'>₹ Rupee</option>
+          </select>
         </div>
-    );
+      </div>
+      <div>
+        <input
+          type='number'
+          value={editableBudget}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          style={{
+            width: '80px',
+            padding: '5px',
+            borderRadius: '3px',
+            border: '1px solid #ccc',
+          }}
+        />
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,36 +3,74 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+
+  const increaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: 10,
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
+  const decreaseAllocation = (name) => {
+    const expense = {
+      name: name,
+      cost: -10,
+    };
 
-    }
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense,
+    });
+  };
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+  const buttonStyle = {
+    padding: '5px 8px', // Make the button a bit smaller
+    marginRight: '5px',
+    cursor: 'pointer',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '50%', // Make the button fully circular
+    fontSize: '14px', // Decrease the font size
+  };
+
+  const decreaseButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: 'red', // Change the background color to red for the "Decrease by" button
+  };
+
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>£{props.cost}</td>
+      <td>
+        <button onClick={() => increaseAllocation(props.name)} style={buttonStyle}>
+          +
+        </button>
+      </td>
+      <td>
+        <button onClick={() => decreaseAllocation(props.name)} style={decreaseButtonStyle}>
+          -
+        </button>
+      </td>
+      <td>
+        <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
+      </td>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
